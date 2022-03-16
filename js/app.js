ece8f64f1e3eccbb7321b60e8dde4cf48920bbeb
Kecar2 <kendrycarvajalsegura>
2022-03-16 14:05:07 +0100

    commitfirst

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 // Tu código aquí
 
+let form = document.forms["messages"];
 
 form.addEventListener('submit', function(e){
-    console.log("Hemos hecho clic en el botón de submit!")
+    console.log("Informacion sobre el objetivo event: ", e)
+    e.preventDefault() // Curioso sobre lo que hace esto? Mira como se comporta el formulario cuando haces clic en "Enviar" si esta línea está comentada.
+
+    let infoCaja = e.target.elements["message"].value;
+
+    document.querySelector(".message-content").textContent = infoCaja;
+
+    console.log("Hemos hecho clic en el botón de submit!", infoCaja)
 
 })
+
+    
